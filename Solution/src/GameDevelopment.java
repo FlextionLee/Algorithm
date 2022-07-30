d2e395df9f66f1b09ca5a35e09c544fb0e5ecd40
FlextionLEE <leeflextion@gmail.com>
2022-07-31 03:44:12 +0900

    알고리즘 스터디 과제 완료

--- Solution/src/GameDevelopment.java
+++ Solution/src/GameDevelopment.java
@@ -10,19 +10,19 @@ public class GameDevelopment {
 		int row;
 		int col;
 		int direction;
-		int sight;
-		int count=0;
+		int count;
 		
 		public User(int row,int col,int direction) {
 			this.row = row;
 			this.col = col;
 			this.direction = direction;
+			this.count = 1;
 		}
 	}
 	
-	//북서남동 으로 델타 선언
+	//북동남서 으로 델타 선언
 	static int dx[] = {-1,0,1,0};
-	static int dy[] = {0,-1,0,1};
+	static int dy[] = {0,1,0,-1};
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -51,6 +51,41 @@ public class GameDevelopment {
 		}
 		
 	
+		int breakCount = 0; //멈춘횟수		
+		int nx = user.row; //row좌표
+		int ny = user.col; //col좌표
+		visited[user.row][user.col] = true; //현재 시작점 방문 체크
+		
+		while(true) {
+			if(breakCount == 4) {
+				//4방향 전부 갈 곳이 없을때 뒤로 갈 수 있다면 위치를 옮김
+				if(map[nx-dx[user.direction]][ny-dy[user.direction]] == 0) {
+					nx = nx-dx[user.direction];
+					ny = ny-dy[user.direction];
+					continue;
+				}
+				//뒤로마저 갈 수 없을때 멈추기
+				break;
+			}
+			//바라보는 방향은 -1이라면 3을 가르키게함 북 -> 서 경우 아니라면 방향을 감소시킴 (델타 참조) 
+			user.direction = (user.direction-1==-1)?user.direction=3:user.direction-1;
+			// 갈 방향 1칸 전진했을때 좌표 생성
+			int tempx = nx + dx[user.direction];
+			int tempy = ny + dy[user.direction];
+			// 배열의 범위를 벗어나거나 방문했거나 바다이거나 경우의 수를 체크하여 멈춘횟수를 증가 시키고 진행
+			if(tempx < 0 || tempx >= n || tempy < 0 || tempy >=m || map[tempx][tempy] == 1 || visited[tempx][tempy] == true) {
+				breakCount++;
+				continue;
+			}
+			//갈 수있다면 방문체크,nx,ny좌표 현재있는 곳으로 바꿔주고 한칸 증가, 4방향중 한방향이라도 갈 수 있었을 상황이었으니 멈춘횟수 초기화 시켜줌
+			visited[tempx][tempy] = true;
+			user.count++;
+			nx = tempx;
+			ny = tempy;
+			breakCount=0;
+		}
+		System.out.println(user.count);
+
 	}
 
 }
