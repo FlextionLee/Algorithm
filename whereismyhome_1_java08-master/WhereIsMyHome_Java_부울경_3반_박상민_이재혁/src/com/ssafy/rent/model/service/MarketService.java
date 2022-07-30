14ee96e60aace15ea9457c97b385be9ba35e19f9
FlextionLEE <leeflextion@gmail.com>
2022-07-31 03:46:49 +0900

    수정

--- whereismyhome_1_java08-master/WhereIsMyHome_Java_부울경_3반_박상민_이재혁/src/com/ssafy/rent/model/service/MarketService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.rent.model.service;
-
-import java.util.List;
-
-import com.ssafy.rent.model.dto.MarketInfo;
-
-public interface MarketService {
-	List<MarketInfo> searchAll(String dong);
-}
--- whereismyhome_1_java08-master/WhereIsMyHome_Java_부울경_3반_박상민_이재혁/src/com/ssafy/rent/model/service/SchoolService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.rent.model.service;
-
-import java.util.List;
-
-import com.ssafy.rent.model.dto.SchoolInfo;
-
-public interface SchoolService {
-	List<SchoolInfo> searchAll(String dong);
-}
