7425c3b02eeee245b55ca28a831fbc5f811c8eb5
FlextionLEE <leeflextion@gmail.com>
2022-08-03 00:02:17 +0900

    no message

--- Solution/src/FunctionDevelopment_PM.java
+++ Solution/src/FunctionDevelopment_PM.java
@@ -1,6 +1,15 @@
-public class DevelopmentFunction_PM {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    public int[] solution(int[] progresses, int[] speeds) {
+public class FunctionDevelopment_PM {
+    public static void main(String[] args) {
+        int[] prog = {95,90,99,99,80,99};
+        int[] speed = {1,1,1,1,1,1};
+        System.out.println(Arrays.toString(solution(prog,speed)));
+    }
+
+    public static int[] solution(int[] progresses, int[] speeds) {
 
         int tmp[] = new int[speeds.length];
 
