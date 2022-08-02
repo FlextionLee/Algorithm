b1ad12bcf9b66e8aaf980ab2175a6afddadc91ce
FlextionLEE <leeflextion@gmail.com>
2022-08-02 23:56:23 +0900

    Daily push

--- Solution/src/Ladder_SWEA1210.java
+++ Solution/src/Ladder_SWEA1210.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class Ladder_SWEA {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ladder_SWEA1210 {
+
+    static int[][] map = new int[100][100];
+    static int[] dx = {0,0,-1};
+    static int[] dy = {-1,1,0};
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int tc = 1; tc<=10 ; tc++){
+            br.readLine();
+
+            for(int i=0; i<100; i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for(int j=0; j<100; j++){
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int y = 0;
+            int x = 99;
+            int idx = 0;
+
+            for(int i=0; i<100; i++){
+                if(map[99][i] == 2){
+                    y = i;
+                    break;
+                }
+            }
+
+            while(true){
+                if(x==0){
+                    break;
+                }
+                for(int i=0;  i<3; i++){
+                    int nx = x+dx[i];
+                    int ny = y+dy[i];
+
+                    if(nx>=0 && ny>=0 && ny<100 && map[nx][ny] == 1) {
+                        x = nx;
+                        y = ny;
+                        map[x-dx[i]][y-dy[i]] = 2;
+                        break;
+                    }
+                }
+            }
+            System.out.println("#"+tc+" "+y);
+        }
+    }
 }
+
--- Solution/src/MuziLive.java
+++ Solution/src/MuziLive.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class MuziLive {
+import java.util.*;
+
+public class MuziLive {
+    public static void main(String[] args) {
+        //독특한 방식의 먹방
+        //회전판에 n개의 음식 각음식은 1부터 번호가 붙어있음
+        //음식을 섭취하는데 일정 시간 소요
+        //1번 부터 먹기 시작
+        // 1초동안만 먹음 쭉 돌다가
+        //먹방을 시작한지 k초 후에 방송 중단
+        //네트워크 정상화 후 다시 방송을 이어갈때, 몇번 음식부터 먹어야하는지 알고싶다.
+        int food[] = {3,1,2};
+        solution(food,5);
+
+    }
+    public static class Food implements Comparable<Food>{
+        int food;
+        int idx;
+        public Food(int food, int idx){
+            this.food = food;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Food o) {
+            return this.food-o.food;
+        }
+    }
+    public static int solution(int[] food_times, int k){
+        Queue<Food> q = new LinkedList<>();
+        List<Food> list = new ArrayList<>();
+
+        for(int i=0; i<food_times.length; i++){
+            list.add(new Food(food_times[i],i+1));
+        }
+
+        Collections.sort(list);
+
+        return 0;
+    }
 }
