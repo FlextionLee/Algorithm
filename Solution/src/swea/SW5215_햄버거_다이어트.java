be2c69d46ef96d4dfd0f495eff0b7391a500d19a
leeflection <leeflection@gmail.com>
2022-08-16 22:17:15 +0900

    SWEA 5215 햄버거 다이어트

--- Solution/src/swea/SW5215_햄버거_다이어트.java
+++ Solution/src/swea/SW5215_햄버거_다이어트.java
@@ -5,63 +5,66 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class SW5215_햄버거_다이어트 {
-    static class Hamburger{
-        int taste;
-        int calory;
-        public Hamburger(int taste, int calory) {
-            this.taste = taste;
-            this.calory = calory;
+    static class ham{
+        int cal;
+        int sco;
+        public ham(int sco, int cal){
+            this.cal = cal;
+            this.sco = sco;
         }
+
     }
-    static int T,N,M,TASTE,CALORY;
-    static Hamburger[] hams;
-    static boolean isSelected[];
-    static int MAX = Integer.MIN_VALUE;
+    static int T,N,limit;
+    static ham[] hams;
+    static int max= Integer.MIN_VALUE;
+    static boolean[] visited;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
 
         for(int t=1; t<=T; t++) {
-            MAX = Integer.MIN_VALUE;
-            StringBuilder sb = new StringBuilder();
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            hams = new Hamburger[N];
-            isSelected = new boolean[N];
+            limit = Integer.parseInt(st.nextToken());
+            hams = new ham[N];
+            visited = new boolean[N];
 
-            for(int i=0; i<N; i++) {
+            for(int i=0; i<N; i++){
                 st = new StringTokenizer(br.readLine());
-                hams[i] = new Hamburger(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+                hams[i] = new ham(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
             }
+
+
             dfs(0);
-            sb.append("#").append(t).append(" ").append(MAX);
-            System.out.println(sb.toString());
+            System.out.println(max);
         }
     }
-    private static void dfs(int index) {
-        if(index == N) {
-            int cal = 0;
-            int tas = 0;
-            for (int i = 0; i < N; i++) {
-                if(isSelected[i]) {
-                    cal += hams[i].calory;
-                    tas += hams[i].taste;
-                }
-            }
-            if(cal >= M) {
-                return;
-            }
-            else {
-                MAX = Math.max(MAX, tas);
-            }
+    private static void dfs(int depth) {
+        if(depth == N){
+            check();
             return;
         }
-        isSelected[index] = true;
-        dfs(index+1);
-        isSelected[index] = false;
-        dfs(index+1);
+
+        visited[depth] = true;
+        dfs(depth+1);
+        visited[depth] = false;
+        dfs(depth+1);
     }
 
+    private static void check() {
+        int sumS = 0;
+        int sumC = 0;
+
+        for(int i=0; i<N; i++){
+            if(visited[i]){
+                sumS += hams[i].sco;
+                sumC += hams[i].cal;
+            }
+        }
+
+        if(sumC<=limit){
+            max = Math.max(max,sumS);
+        }
+    }
 }
 
