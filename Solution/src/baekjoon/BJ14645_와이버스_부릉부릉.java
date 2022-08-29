8b93906f4648e68b769b2bed37ad44b0046b1a8a
leeflection <leeflection@gmail.com>
2022-08-29 18:14:02 +0900

    BJ 14645 와이버스_부릉부릉

---
+++ Solution/src/baekjoon/BJ14645_와이버스_부릉부릉.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BJ14645_와이버스_부릉부릉 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m =sc.nextInt();
+
+        for(int i=0; i<n; i++){
+
+                String str = sc.next();
+
+        }
+        System.out.println("비와이");
+    }
+
+}
