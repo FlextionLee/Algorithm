4678f1218dd77f585da66f51baf148142cc6fee2
leeflection <leeflection@gmail.com>
2022-09-06 21:32:28 +0900

    백준 2743 단어 길이 재기

---
+++ Solution/src/baekjoon/BJ2743_단어_길이_재기.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ2743_단어_길이_재기 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().length());
+    }
+}
