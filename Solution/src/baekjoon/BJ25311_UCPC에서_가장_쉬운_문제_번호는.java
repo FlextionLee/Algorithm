04181fac8c55e0de61359fa5ce72c4847e377e91
leeflection <leeflection@gmail.com>
2022-09-08 21:09:35 +0900

    백준  25311 UCPC 에서 가장 쉬운 문제 번호는

---
+++ Solution/src/baekjoon/BJ25311_UCPC에서_가장_쉬운_문제_번호는.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BJ25311_UCPC에서_가장_쉬운_문제_번호는 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println('A');
+    }
+}
