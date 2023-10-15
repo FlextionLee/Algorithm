8c28de7a04b03e0483c287b07416592339f9ae03
leeflection <leeflection@gmail.com>
2023-10-15 16:55:59 +0900

    백준 1939 중량제한

--- Solution/src/baekjoon/BJ1939_중량제한.java
+++ Solution/src/baekjoon/BJ1939_중량제한.java
@@ -9,69 +9,68 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BJ1939_중량제한 {
-        static int N, M, A, B;
-        static int[] dist;
-        static ArrayList<Node> node[];
-        static int INF = Integer.MAX_VALUE;
-
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            dist = new int[N + 1];
-            Arrays.fill(dist, -1);
-            node = new ArrayList[N + 1];
-            for (int i = 0; i <= N; i++)
-                node[i] = new ArrayList<>();
-
-            for (int i = 0; i < M; i++) {
-                st = new StringTokenizer(br.readLine());
-                int from = Integer.parseInt(st.nextToken());
-                int to = Integer.parseInt(st.nextToken());
-                int weight = Integer.parseInt(st.nextToken());
-                node[from].add(new Node(to, weight));
-                node[to].add(new Node(from, weight));
-            }
+    static int ans;
+    static ArrayList<City>[] list;
+    static boolean[] check;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        list = new ArrayList[n+1];
+        for(int i=1; i<n+1; i++) {
+            list[i] = new ArrayList<>();
+        }
+        int left =0;
+        int right =0;
+        for(int i=0; i<m; i++) {
             st = new StringTokenizer(br.readLine());
-            A = Integer.parseInt(st.nextToken());
-            B = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
-            Dijkstra();
-
-            System.out.println(dist[B]);
-
+            list[a].add(new City(b,w));
+            list[b].add(new City(a,w));
+            right = Math.max(right, w); // 그래프 최대 중량 구하기
         }
 
-        public static void Dijkstra() {
-            PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> (o2.weight - o1.weight));
-            pq.offer(new Node(A, INF));
-            dist[A] = INF;
-            while (!pq.isEmpty()) {
-                int from = pq.peek().to;
-                int weight = pq.peek().weight;
-                pq.poll();
-                if (dist[from] > weight) continue; // 이미 중량이 크면 update할 필요가 없으므로
+        st = new StringTokenizer(br.readLine());
+        int from = Integer.parseInt(st.nextToken());
+        int to = Integer.parseInt(st.nextToken());
 
-                for (int i = 0; i < node[from].size(); i++) {
-                    int to = node[from].get(i).to;
-                    int weightTo = Math.min(weight, node[from].get(i).weight);
-                    if (weightTo > dist[to]) {
-                        dist[to] = weightTo;
-                        pq.offer(new Node(to, weightTo));
-                    }
-                }
+        while(left<= right) {
+            int mid = (left+right)/2;
+            ans = -1;
+            check = new boolean[n+1];
+            dfs(from,to,mid);
+            if(ans != -1) { // 이동이 가능하면 중량 올리기
+                left = mid+1;
+            }else { // 이동 불가능하면 중량 줄이기
+                right = mid-1;
             }
         }
+        System.out.println(right);
+    }
 
-        static class Node {
-            int to;
-            int weight;
-
-            public Node(int to, int weight) {
-                this.to = to;
-                this.weight = weight;
+    static void dfs(int pos, int target, int limit) {
+        if(pos == target) {
+            ans = pos;
+            return;
+        }
+        check[pos]= true;
+        for(City c : list[pos]) {
+            if(!check[c.to] && limit <= c.w ) {
+                dfs(c.to, target, limit);
             }
         }
     }
+}
+class City{
+    int to;
+    int w;
+
+    public City(int to, int w) {
+        this.to = to;
+        this.w = w;
+    }
+}
