16cfab3736085cc8e972f5e8b8853ff02e178711
leeflection <leeflection@gmail.com>
2023-11-05 16:42:02 +0900

    백준 9095 1,2,3 더하기

---
+++ Solution/src/baekjoon/BJ9095_123더하기.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ9095_123더하기 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i=4; i<11; i++){
+            dp[i] = dp[i-3]+dp[i-2]+dp[i-1];
+        }
+        for(int t=0; t<T; t++){
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+    }
+}
