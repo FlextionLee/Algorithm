bd5e8737ad4bee6307dfd5d21c57a1ba159cb994
leeflection <leeflection@gmail.com>
2023-11-06 18:49:53 +0900

    백준 11057 오르막수

---
+++ Solution/src/baekjoon/BJ11057_오르막수.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ11057_오르막수 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n+1][10];
+        for(int i=0; i<10; i++){
+            dp[1][i] = 1;
+        }
+
+        for(int i=2; i<=n; i++){
+            for(int j=0; j<10; j++){
+                for(int k=0; k<=j; k++){
+                    dp[i][j] += dp[i-1][k];
+                    dp[i][j] %= 10007;
+                }
+            }
+        }
+        int sum = 0;
+        for(int i=0; i<10; i++){
+            sum += dp[n][i];
+        }
+        System.out.println(sum%10007);
+    }
+}
